a0421748f29b0f1a57914fee79f27b81a62629a7
muralianand12345 <71955467+muralianand12345@users.noreply.github.com>
2022-02-13 09:00:38 +0530

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,21 @@
 module.exports = {
     name: 'ready',
     execute(client) {
+        const activities = [
+            "Your Queries 24/7!",
+            "Your Queries 24/7!",
+            "Your Queries 24/7!",
+            "Your Ticket",
+            "Music and Vibing",
+            "OwnerPlayz's Command"
+          ];
         console.log(`${client.user.tag} Bot is ready to rock n roll!`);
         console.log('Developed For EliteX Rp <3');
-        //activity
-        client.user.setActivity(`Your Queries 24/7!`, { type: "LISTENING" });     
-        
+
+        setInterval(() => {
+            const randomIndex = Math.floor(Math.random() * (activities.length - 1) + 1);
+            const newActivity = activities[randomIndex];
+            client.user.setActivity(newActivity,{ type : "LISTENING"});
+        }, 5000);
     },
 };
