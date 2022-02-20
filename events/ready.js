d05148373207cb2e5d71b4465216e827556b7fa1
muralianand12345 <71955467+muralianand12345@users.noreply.github.com>
2022-02-20 09:37:49 +0530

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,7 @@
+const moment = require('moment');
+const tz = require('moment-timezone');
+//const chalk = require('chalk');
+
 module.exports = {
     name: 'ready',
     execute(client) {
@@ -12,10 +16,36 @@ module.exports = {
         console.log(`${client.user.tag} Bot is ready to rock n roll!`);
         console.log('Developed For EliteX Rp <3');
 
-        setInterval(() => {
-            const randomIndex = Math.floor(Math.random() * (activities.length - 1) + 1);
-            const newActivity = activities[randomIndex];
-            client.user.setActivity(newActivity,{ type : "LISTENING"});
-        }, 5000);
+        const needDatenTime = client.config.DateNTime;
+        if (needDatenTime == "true") {
+            console.log('Date and Time has been enabled');
+            const TIMEZONE = client.config.TIMEZONE;
+            const FORMAT = client.config.FORMATDate;
+            const CHANNEL_ID = client.config.dateChannel;
+            const UPDATE_INTERVAL = client.config.UPDATE_INTERVAL;
+            
+            setInterval(() => {
+                const randomIndex = Math.floor(Math.random() * (activities.length - 1) + 1);
+                const newActivity = activities[randomIndex];
+                client.user.setActivity(newActivity,{ type : "LISTENING"});
+            }, 5000);
+
+            const timeNow = moment().tz(TIMEZONE).format(FORMAT);
+            const clockChannel = client.channels.cache.get(CHANNEL_ID);
+            clockChannel.edit({ name: `🕒 ${timeNow}` }, 'Clock update').catch(console.error);
+        
+            setInterval(() => {
+                const timeNowUpdate = moment().tz(TIMEZONE).format(FORMAT);
+                clockChannel.edit({ name: `🕒 ${timeNowUpdate}` }, 'Clock update').catch(console.error);
+            }, UPDATE_INTERVAL);
+
+        } else if (needDatenTime == "false") {
+            console.log('Date and Time has been disabled');
+        } else {
+            console.log('Error Has been occured at Time and Date!');
+        }
+
+        
+
     },
 };
