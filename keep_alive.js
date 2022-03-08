16a70c3e4168f17cef353504b57b0ad3e7a9b36c
Murali Anand <71955467+muralianand12345@users.noreply.github.com>
2022-03-08 22:16:17 +0530

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.write("I'm alive 10-8");
+  res.write("Your Replit Ticket Bot By Murali Anand is Alive and ready to help!");
   res.end();
 }).listen(8080);
