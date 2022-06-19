6cce8e14897dc48731f2ef41e55ea8ad8923ef90
Murali Anand <71955467+muralianand12345@users.noreply.github.com>
2022-06-19 11:05:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,14 +4,22 @@ const {
     Collection,
     Intents
 } = require('discord.js');
-const config = require('./config.json');
-const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS],
-});
-const Discord = require('discord.js');
 
+//config file for channels and owner id
+const config = require('./config.json');
+
+
+const client = new Client({
+    intents: [Intents.FLAGS.DIRECT_MESSAGES,Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS],
+});
+
+//Discord
+const Discord = require('discord.js');
 client.discord = Discord;
+//config
 client.config = config;
+
+//command file auto read
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -20,6 +28,7 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command);
 };
 
+//events
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
@@ -27,23 +36,38 @@ for (const file of eventFiles) {
     client.on(event.name, (...args) => event.execute(...args, client));
 };
 
+//interaction 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
+
     const command = client.commands.get(interaction.commandName);
     if (!command) return;
 
     try {
         await command.execute(interaction, client, config);
-
     } catch (error) { //auto error 
-        console.log(error);
-
+        console.error(error);
         return interaction.reply({
             content: 'There was an error while executing this command!',
             ephemeral: true
         });
     };
 });
+
+
+try{
+    client.on('message', async message => {
+        const mention = new RegExp(`^<@!?${client.user.id}>( |)$`);
+        if (message.content.match(mention)) {
+            message.reply({ content: "Hello! How can I help you?" })
+        }
+    });
+} catch(err) {
+    const errTag = client.config.errTag;
+    const errChan = client.config.errorLog;
+    const errorSend = client.channels.cache.get(errChan);
+    errorSend.send(`**ERROR!** ${errTag} \n${err}\nCommand: \`Mention Reply\``);
+}
 
 require("dotenv").config();
 const Token = process.env.TOKEN;
